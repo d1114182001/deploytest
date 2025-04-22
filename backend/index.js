2da0c27b62c7df8d74c5344fd47ad5d94920cf89
d1114182001 <d1114182001@gm.lhu.edu.tw>
2025-04-22 22:36:14 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,14 @@ const db = mysql.createConnection({
   database: process.env.DB_NAME,
 });
 
+console.log('DB Config:', {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
 db.connect((err) => {
   if (err) {
     console.error("Database connection failed:", err.message);
